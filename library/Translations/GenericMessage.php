4d4dff35009ae6fd0acd036742fda58992e029f0
MoNoLidThZ <admin@monolidthz.com>
2015-05-06 23:06:55 +0700

    Revert "Initial Commit"
    This reverts commit cd1842258cbfa95b494bba37d104b3ac7a78eed1.

--- library/Translations/GenericMessage.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Translations;
-class GenericMessage{
-	public static function GetUsername(){
-		return $_SESSION['FacebookUserID'] ? $_SESSION['FacebookUserID']['name'] ? "TR:General-User_NotLoggedIn";
-	}
-}
